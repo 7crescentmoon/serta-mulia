3a37c63320ad362aa20a2de83ff04d58f346f485
moon <mirfan1537.gmail.com>
2024-05-22 11:45:01 +0700

    test

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const routes = require('../server/routes');
 const loadModel = require('../services/loadModel');
 const InputError = require('../exceptions/InputError');
-const ClientError = require('../exceptions/ClientError');
- 
+
 (async () => {
     const server = Hapi.server({
         port: 3000,
@@ -15,15 +15,15 @@ const ClientError = require('../exceptions/ClientError');
             },
         },
     });
- 
+
     const model = await loadModel();
     server.app.model = model;
- 
+
     server.route(routes);
- 
+
     server.ext('onPreResponse', function (request, h) {
         const response = request.response;
- 
+
         if (response instanceof InputError) {
             const newResponse = h.response({
                 status: 'fail',
@@ -32,7 +32,7 @@ const ClientError = require('../exceptions/ClientError');
             newResponse.code(response.statusCode)
             return newResponse;
         }
- 
+
         if (response.isBoom) {
             const newResponse = h.response({
                 status: 'fail',
@@ -41,10 +41,10 @@ const ClientError = require('../exceptions/ClientError');
             newResponse.code(response.output.statusCode)
             return newResponse;
         }
- 
+
         return h.continue;
     });
- 
+
     await server.start();
     console.log(`Server start at: ${server.info.uri}`);
 })();
